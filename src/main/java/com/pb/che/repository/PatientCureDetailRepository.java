7dbc8aee17f3305664dcb2c73523775a419a047c
rongo360 <714179558@qq.com>
2019-05-18 15:00:24 +0800

    添加12

--- src/main/java/com/pb/che/repository/PatientCureDetailRepository.java
+++ src/main/java/com/pb/che/repository/PatientCureDetailRepository.java
@@ -3,8 +3,10 @@ package com.pb.che.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pb.che.entity.PatientCureDetail;
+
 @Repository
-public interface PatientCureDetailRepository extends JpaRepository<PatientCureDetailRepository,
+public interface PatientCureDetailRepository extends JpaRepository<PatientCureDetail,
 Integer>
 {
 
--- src/main/java/com/pb/che/service/PatientService.java
+++ src/main/java/com/pb/che/service/PatientService.java
@@ -1,10 +1,16 @@
 package com.pb.che.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.pb.che.entity.Patient;
+import com.pb.che.entity.PatientCondition;
+import com.pb.che.entity.PatientCureDetail;
+import com.pb.che.repository.PatientConditionRepository;
+import com.pb.che.repository.PatientCureDetailRepository;
 import com.pb.che.repository.PatientRepository;
 import com.pb.che.utils.ResultObject;
 
@@ -14,8 +20,12 @@ public class PatientService
 
 	@Autowired
 	private PatientRepository patientRepository;
+	@Autowired
+	private  PatientCureDetailRepository patientCureDetailRepository;
+	@Autowired
+	private  PatientConditionRepository patientConditionRepository;
 	
-	
+	@Transactional
 	public ResultObject save(String data){
 		ResultObject resultObject=new ResultObject();
 		
@@ -23,7 +33,13 @@ public class PatientService
 	
 		patientRepository.save(patient);
 		
+		PatientCondition condition=new PatientCondition();
 		
+		patientConditionRepository.save(condition);
+		
+		PatientCureDetail cureDetail=new PatientCureDetail();
+		
+		patientCureDetailRepository.save(cureDetail);
 		return resultObject;
 		
 	}
